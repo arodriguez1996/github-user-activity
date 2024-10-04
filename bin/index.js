4ea8be395b0f95577b5c45ea16686b4980d764ba
arodriguez1996 <arodriguezv22@gmail.com>
2024-10-03 20:19:14 -0500

    init cli

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
+const [_, __, username] = process.argv;
 
-console.log('bichus palus')
+
+if (!username) {
+    console.log('Please provide a username');
+}
+
+console.log('bichus palus' + username);
