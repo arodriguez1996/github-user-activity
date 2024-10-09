f12333026f8d77ffca830bdc9de8a4f0e2b99f7b
arodriguez1996 <arodriguezv22@gmail.com>
2024-10-09 18:05:12 -0500

    enable .env

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
+import 'dotenv/config'
+// import { get } from 'axios';
 
 const [_, __, username] = process.argv;
+
+console.log(process.env.GITHUB_KEY);
 
 
 if (!username) {
     console.log('Please provide a username');
 }
+
+// get(`https://api.github.com/users/arodriguez1996/events`).then(({ data }))
 
 console.log('bichus palus' + username);
